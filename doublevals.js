218ad6289d01c09c2d934d1f8de68bc1b3f21c7f
Colin Stanford <j.colin.stanford@gmail.com>
2025-10-06 17:49:30 -0600

    Double array values solution

---
+++ doublevals.js
@@ -0,0 +1,14 @@
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+// [1, 2, 3] --> [2, 4, 6]
+
+testArr = [45, 389, 55]
+
+function maps(x) {
+
+    return x.map((val) => val * 2)
+
+}
+
+console.log(maps(testArr))
