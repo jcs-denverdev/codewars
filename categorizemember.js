6a612fd060ab151c67f61306492ba5a306266a48
Colin Stanford <j.colin.stanford@gmail.com>
2025-10-15 17:35:12 -0600

    top solution added to comments

--- categorizemember.js
+++ categorizemember.js
@@ -28,6 +28,9 @@ function openOrSenior (data) {
         }
     }
     return data
+
+    // Codewars top solution
+    // return data.map(([age, handicap]) => (age > 54 && handicap > 7) ? 'Senior' : 'Open')
 }
 
 console.log(openOrSenior([[45, 12],[55,21],[19, -2],[104, 20]]))
