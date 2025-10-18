b1d352a19dc9407bc0e14082a7170c12037268cc
Colin Stanford <j.colin.stanford@gmail.com>
2025-10-18 10:26:56 -0600

    challenge file created and prep

---
+++ disemvowel.js
@@ -0,0 +1,17 @@
+// Description:
+// Trolls are attacking your comment section! A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+// Your task is to write a function that takes a string and return a new string with all vowels removed.
+// Note: for this kata y isn't considered a vowel.
+
+//Parameters: your function should take a string as a parameter. do you need to check if the user has given a different data type? Or an empty string?
+//Returns: return the given string with vowels removed, not including 'y'
+//Examples: the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+//Pseudocode: 
+
+
+function disemvowel(str) {
+
+
+
+}
