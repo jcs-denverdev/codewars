e0d9ce22c3f943f9d6808cb8dfd572f415ab9941
Colin Stanford <j.colin.stanford@gmail.com>
2025-10-22 21:16:32 -0600

    foreach always returns undefined, trying map

--- abbreviatename.js
+++ abbreviatename.js
@@ -14,8 +14,11 @@
 function abbrevName(name){
 
     let a = name.split(' ');
-    return `${a[0][0].toUpperCase()}.${a[1][0].toUpperCase()}`
 
+    a = a.map((letter) => letter[0]).join('.').toUpperCase()
+
+    return a
+    // return `${a[0][0].toUpperCase()}.${a[1][0].toUpperCase()}`
 }
 
-console.log(abbrevName("sam harris"));
+console.log(abbrevName("s harris booty"));
