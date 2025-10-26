e4d58a7564d61108fd746fdc72d7824c790bbd2d
Colin Stanford <j.colin.stanford@gmail.com>
2025-10-26 16:49:18 -0600

    for loop with includes method solution

--- doesarrcontainx.js
+++ doesarrcontainx.js
@@ -4,14 +4,24 @@
 
 // Return true if the array contains the value, false if not.
 
-// Parameters: 
-// Returns: 
+// Parameters: an array, which can contain nums or strs, and a value
+// Returns: true if a contains x, false if not
 // Examples: 
+    // ([66, 101], 66) ==> true
+    // ([101, 45, 75, 105, 99, 107], 107) ==> true
+    // (['t', 'e', 's', 't'], 'e') ==> true
+    // (['what', 'a', 'great', 'kata'], 'kat') ==> false
 // Pseudocode: 
-
+// loop through a
+// if a includes x, return true
 
 function check(a, x) {
 
+    for (let i = 0; i < a.length; i++) {
+        return a.includes(x)
+    }
 
-    
 }
+
+console.log(check([30, "ireland", "beer", "7"], 7))
+
