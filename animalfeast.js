f6456bf9e4ef8e3371136265c3e0aeb45f0bbe6a
Colin Stanford <j.colin.stanford@gmail.com>
2025-10-29 17:32:29 -0600

    last letters have to match too

--- animalfeast.js
+++ animalfeast.js
@@ -16,8 +16,8 @@
 
 function feast(beast, dish) {
 
-    return beast[0] == dish[0] ? true : false
+    return beast[0] == dish[0] && beast[beast.length-1] == dish[dish.length-1] ? true : false
 
 }
 
-console.log(feast("brown bear", "dear claw"))
+console.log(feast("brown bear", "bear claw"))
