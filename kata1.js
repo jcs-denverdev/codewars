64acf2ec8392c24e6b64adf029d4012595a4ef6e
Colin Stanford <j.colin.stanford@gmail.com>
2025-11-03 15:23:29 -0700

    another 8kyu kata file

--- kata1.js
+++ kata1.js
@@ -1,4 +1,4 @@
-// Kata: Sum Arrays
+// 8 kyu Kata: Sum Arrays
 // Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
 // You can assume that you are only given numbers.
 // You cannot assume the size of the array.
