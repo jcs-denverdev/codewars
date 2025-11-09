caa4e4ee225d8227f116e1beb010f7647c4eb947
Colin Stanford <j.colin.stanford@gmail.com>
2025-11-09 15:39:04 -0700

    mucking up the file with nonsense attempts

--- checkExams.js
+++ checkExams.js
@@ -15,12 +15,47 @@
     //  ["a", "a", "b", "c"]   ["a", "a", "b", "c"]  →     16
     //  ["b", "c", "b", "a"]   ["" , "a", "a", "c"]  →     0
 //Pseudocode
-
+// loop through the correct answers
+// loop through student answers
+// if the element in one array matches the element in the other, add 4 to result, otherwise subtract 1 from result
 
 function checkExam(array1, array2) {
 
+    let result = 0;
 
+    // // first loop for the answer key array
+    // for (let i = 0; i < array1.length; i++) {
+
+    //     // second loop for the student's answers array
+    //     for (let j = 0; j < array2.length; j++) {
+
+    //         if (j === i) {
+    //             result += 4
+    //         } else if (j === "" || j === " ") {
+    //             result += 0
+    //         } else {
+    //             result -= 1
+    //         }
+    //     }
+    // }
+
+    // without a nested for loop, check char at indices
+    if (array1[0] === array2[0] || array1[1] === array2[1] || array1[2] === array2[2] || array1[3] === array2[3]) {
+        result += 4
+    } else if (array2[0] === "" || array2[1] === "" || array2[2] === "" || array2[3] === "") {
+        result += 0
+    } else {
+        result -= 1
+    }
+
+    // check if student score has dropped below 0
+    if (result < 0) {
+        result = 0
+    }
+
+    // return student result
+    return result
 
 }
 
-console.log(checkExam())
+console.log(checkExam(["a", "a", "b", "c"],["a", "a", "b", "c"]))
