37afc0964e5ca870c67c406dd146bf4b71f7da96
Colin Stanford <j.colin.stanford@gmail.com>
2025-11-11 17:45:04 -0700

    sort solution

--- sort.js
+++ sort.js
@@ -1,5 +1,15 @@
 function sorter (textbooks) {
-    return textbooks.sort();
+    return textbooks.sort((a,b) => {
+        const bookA = a.toUpperCase();
+        const bookB = b.toUpperCase();
+        if (bookA < bookB) {
+            return -1;
+        }
+        if (bookA > bookB) {
+            return 1;
+        }
+        return 0;
+    })
 }
 
 console.log(sorter(['Algebra', 'Geometry', 'english', 'history']))
