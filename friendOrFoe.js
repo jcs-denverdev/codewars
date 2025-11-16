2bc45e18b92a149dbc6f3ccf3a828530fe50c0f3
Colin Stanford <j.colin.stanford@gmail.com>
2025-11-16 16:26:22 -0700

    committed new challenge file with prep

---
+++ friendOrFoe.js
@@ -0,0 +1,27 @@
+// Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+// Input strings will only contain letters.
+// Note: keep the original order of the names in the output.
+
+//Params
+// an array of strings
+//Returns
+// an array with only names/strings containing exactly four letters
+//Examples
+    // Input = ["Ryan", "Kieran", "Jason", "Yous"]
+    // Output = ["Ryan", "Yous"]
+
+    // Input = ["Peter", "Stephen", "Joe"]
+    // Output = []
+//Pseudocode
+// 
+
+function friend (friends) {
+
+    
+
+}
+
+console.log(friend(["Ryan", "Kieran", "Jason", "Yous"]))
