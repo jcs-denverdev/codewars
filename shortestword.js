a9434bed04cf837c1acb8f4e93b0bacd87878662
Colin Stanford <j.colin.stanford@gmail.com>
2025-11-25 13:42:33 -0700

    prepping the challenge

---
+++ shortestword.js
@@ -0,0 +1,17 @@
+// P.R.E.P - for every codewars!
+// parameters, returns, examples, pseudo code
+
+// Simple, given a string of words, return the length of the shortest word(s).
+// String will never be empty and you do not need to account for different data types.
+
+function findShort(s) {
+
+    //P: singular parameter, s, represents a string of words that will be passed to this function. s will never be an empty string, and will always be a string
+    //R: return the length of the shortest word
+    //E: "Let's travel abroad shall we" --> shortest word is "we" --> length 2
+        // "turns out random test cases are easier than writing out basic ones" --> shortest word is "out", "are" --> length 3
+    
+    //P: 
+}
+
+console.log(findShort("bitcoin take over the world maybe who knows perhaps"))
