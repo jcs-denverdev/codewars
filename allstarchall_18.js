6e7ba3134250deeb96a2a8a55c724b5a33bc9c1a
Colin Stanford <j.colin.stanford@gmail.com>
2025-11-27 11:52:14 -0700

    loop through str, add one to count each time letter found

--- allstarchall_18.js
+++ allstarchall_18.js
@@ -1,10 +1,5 @@
 // Create a function that accepts a string and a single character, and returns an integer of the count of occurrences the 2nd argument is found in the first one.
-
 // If no occurrences can be found, a count of 0 should be returned.
-
-// ("Hello", 'o')  =>  1
-// ("Hello", 'l')  =>  2
-// ("", 'z')       =>  0
 
 // Notes
 // The first argument can be an empty string
@@ -16,8 +11,21 @@
 // Examples: ("Hello", 'o')  =>  1
     // ("Hello", 'l')  =>  2
     // ("", 'z')       =>  0
-// Pseudocode
+// Pseudocode:
+// check str to see if it includes letter
+// if str includes letter, count ++, keep checking, count++
+// return the count
 
 function strCount(str, letter){  
-    
+
+    let count = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === letter) {
+            count++;
+        }
+    }
+    return count
 }
+
+console.log(strCount("Hello", "l"))
