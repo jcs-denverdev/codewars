2a62f6c8160d62427e9d379cf609e214495d3f48
Colin Stanford <j.colin.stanford@gmail.com>
2025-12-01 18:39:10 -0700

    challenge directions and prep

---
+++ abbreviatename.js
@@ -0,0 +1,17 @@
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+
+//Parameters: function takes a name, presumably a string with two names separated by a space
+//Returns: two capital letters, separated by a period
+//Examples: 
+    // Sam Harris => S.H
+    // patrick feeney => P.F
+//Pseudocode:
+
+
+function abbrevName(name){
+
+    
+
+}
