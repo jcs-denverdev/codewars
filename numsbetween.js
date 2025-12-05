4f21bd50b5e374e21fbb83e495690bb9f48d23e4
Colin Stanford <j.colin.stanford@gmail.com>
2025-12-05 13:10:38 -0700

    challenge added and prep

---
+++ numsbetween.js
@@ -0,0 +1,17 @@
+// Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+
+//Params: two integers, where a < b
+//Returns: an array of all integers between the to give integers, inclusive
+//Examples:
+    // a = 1
+    // b = 4
+    // --> [1, 2, 3, 4]
+//Pseudocode:
+
+function between (a,b) {
+
+
+
+}
+
+console.log(between(1, 4))
