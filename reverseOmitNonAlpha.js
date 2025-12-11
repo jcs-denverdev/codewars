7a2a82be57a09be2d59a02df7172efea46e54706
Colin Stanford <j.colin.stanford@gmail.com>
2025-12-11 16:19:27 -0700

    challenge added with prep

---
+++ reverseOmitNonAlpha.js
@@ -0,0 +1,24 @@
+// Task
+// Given a string str, reverse it and omit all non-alphabetic characters.
+
+// Example
+// For str = "krishan", the output should be "nahsirk".
+
+// For str = "ultr53o?n", the output should be "nortlu".
+
+// Input/Output
+// [input] string str
+// A string consists of lowercase latin letters, digits and symbols.
+
+// [output] a string
+
+//Params
+//Returns
+//Examples
+//Pseudocode
+
+function reverseLetter(str) {
+
+}
+
+console.log(reverseLetter("ultra"))
