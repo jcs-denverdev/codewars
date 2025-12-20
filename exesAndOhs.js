827d10a9a4cc4b78436b8ce15c97f0362c555840
Colin Stanford <j.colin.stanford@gmail.com>
2025-12-20 14:03:47 -0700

    reversewords finished, added new challenge and prep

---
+++ exesAndOhs.js
@@ -0,0 +1,22 @@
+// Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+//Params
+// given a string containing any character
+//Returns
+// checking for x's and o's. if the string has the same amount of each letter, return true. otherwise, return false
+// Examples input/output:
+    // XO("ooxx") => true
+    // XO("xooxx") => false
+    // XO("ooxXm") => true
+    // XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+    // XO("zzoo") => false
+//Pseudocode
+// first, check mdn for string methods. if none, we can convert it to an array and use includes maybe
+
+function XO (str) {
+
+
+
+}
+
+console.log(XO("xooxx"))
