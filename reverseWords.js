7f2483d39dfbe4f5eb4b60fae135683eedc68825
Colin Stanford <j.colin.stanford@gmail.com>
2025-12-19 17:19:00 -0700

    challenge prep added

---
+++ reverseWords.js
@@ -0,0 +1,24 @@
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+//Params
+// a single string
+//Returns
+// reverse each word in the string, rather than reversing the entire string as a whole
+// Examples
+    // "This is an example!" ==> "sihT si na !elpmaxe"
+    // "double  spaces"      ==> "elbuod  secaps"
+//Pseudocode
+
+
+function reverseWords(str) {
+
+    // Reverse entire string as a whole
+    // return str.split('')
+    //           .reverse()
+    //           .join('')
+
+    
+
+}
+
+console.log(reverseWords("This is an example!"))
