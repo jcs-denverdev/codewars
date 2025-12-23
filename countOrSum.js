5c83f5bd586ac0b6717545657f56a1f5d120dff3
Colin Stanford <j.colin.stanford@gmail.com>
2025-12-23 11:20:08 -0700

    committing new challenge file with prep

---
+++ countOrSum.js
@@ -0,0 +1,28 @@
+// Given an array of integers.
+
+// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+// If the input is an empty array or is null, return an empty array.
+
+// Example
+// For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+//Params
+// an array of integers, which can be positive or negative
+//Returns
+// return an array based on parameter array, in which the first element is the COUNT of any positive numbers within it, and the second element is the SUM of any negative numbers. 0 is neither + nor -
+//Examples
+// [1,2,3,-1,-2] --> [3,-3]
+//Pseudocode
+// loop through the input parameter array
+// if element > 0, count++
+// if element < 0, sum+element
+// return [count, sum]
+
+function countPositivesSumNegatives(input) {
+
+    
+
+}
+
+console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]))
