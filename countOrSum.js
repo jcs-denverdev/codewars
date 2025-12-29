33a85886a88fc7e7d151ab780f80cadde6d6e95a
Colin Stanford <j.colin.stanford@gmail.com>
2025-12-29 11:29:29 -0700

    function counts and sums, but doesnt handle zero

--- countOrSum.js
+++ countOrSum.js
@@ -21,8 +21,21 @@
 
 function countPositivesSumNegatives(input) {
 
-    
+    let count = 0;
+    let sum = 0;
+
+    for (let i = 0; i < input.length; i++) {
+        if (input[i] > 0) {
+            count++
+        } else if (input[i] < 0) {
+            sum += input[i]
+        } else {
+            return []
+        }
+    }
+
+    return [count, sum]
 
 }
 
-console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]))
+console.log(countPositivesSumNegatives([1,2,3,-1,-2]))
