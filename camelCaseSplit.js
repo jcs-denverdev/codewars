0c86869c53d3e64b2b8f67f65c968c4ba079fdb1
Colin Stanford <j.colin.stanford@gmail.com>
2026-01-09 17:12:57 -0700

    committing new challenge file and prep

---
+++ camelCaseSplit.js
@@ -0,0 +1,17 @@
+// Complete the solution so that the function will break up camel casing, using a space between words.
+
+//Params
+//Returns
+//Examples
+// "camelCasing"  =>  "camel Casing"
+// "identifier"   =>  "identifier"
+// ""             =>  ""
+//Pseudocode
+
+function solution(string) {
+
+    
+
+}
+
+console.log(solution("camelCasing"))
