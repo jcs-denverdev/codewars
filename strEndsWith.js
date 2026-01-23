e8f87827269bbd33d9bec844dd6001c569ef7fe4
Colin Stanford <j.colin.stanford@gmail.com>
2026-01-22 17:05:10 -0700

    committing new challenge file and prep

---
+++ strEndsWith.js
@@ -0,0 +1,18 @@
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+//Params
+//Returns
+//Examples:
+    // Inputs: "abc", "bc"
+    // Output: true
+    // Inputs: "abc", "d"
+    // Output: false
+//Pseudocode
+
+function solution (str, ending) {
+
+
+
+}
+
+console.log(solution("abc", "bc"))
