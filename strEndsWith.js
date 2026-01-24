29fa90e577f20fa1552fca3300cd7dfb6fc9c2d3
Colin Stanford <j.colin.stanford@gmail.com>
2026-01-23 17:08:26 -0700

    endsWith solution

--- strEndsWith.js
+++ strEndsWith.js
@@ -1,17 +1,21 @@
 // Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
 
 //Params
+// two string parameters
 //Returns
+// true if second argument is found at the end of the first arg
 //Examples:
     // Inputs: "abc", "bc"
     // Output: true
     // Inputs: "abc", "d"
     // Output: false
 //Pseudocode
+// if str length is 0, return false
+// if str.endsWith ending, return true
 
 function solution (str, ending) {
 
-
+    return str.endsWith(ending)
 
 }
 
