8a670c98bcdbef30539834e8c053950274daaa16
Colin Stanford <j.colin.stanford@gmail.com>
2026-03-16 17:24:13 -0600

    new challenge file with prep

---
+++ palindrome.js
@@ -0,0 +1,27 @@
+// Write a function that checks if a given string (case insensitive) is a palindrome.
+
+// A palindrome is a word, number, phrase, or other sequence of symbols that reads the same backwards as forwards, such as madam or racecar.
+
+//Parameters
+// a string parameter, case insensitive
+//Returns
+// true if the string is a palindrome, false if not
+//Examples
+    // "abba" -> true
+    // "madam" -> true
+    // "hello" -> false
+    // "mADaM" -> true
+//Pseudocode
+// loop through string forwards
+
+// .at(index) returns a new string consisting of the single utf-16 code unit located at the specified offset
+// .charAt(index) returns a new string consisting of the single utf-16 code unit at the given index
+// .substring(startIndex, endIndex) returns the part of this string from the start index up to and excluding the end index, or to the end of the string if no end index supplied
+
+function isPalindrome(x) {
+
+    
+
+}
+
+console.log(isPalindrome("abba"))
