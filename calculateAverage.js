ba1eec1389b8b1815649b6b4e4faf720dbd2e525
Colin Stanford <j.colin.stanford@gmail.com>
2026-04-15 20:06:14 -0600

    committed new challenge file and prep

---
+++ calculateAverage.js
@@ -0,0 +1,18 @@
+// Write a function which calculates the average of the numbers in a given array.
+
+// Note: Empty arrays should return 0.
+
+//Params
+// you are given an array of numbers
+//Returns
+// return the average of the numbers within given array
+//Examples
+// [2, 5, 8] => (2+5+8)/3 => 5
+//Pseudocode
+// sum each number then divide by count
+
+function findAverage (array) {
+
+    
+    return 0;
+}
--- tripleDouble.js
+++ tripleDouble.js
@@ -20,7 +20,7 @@
 
 function tripleTrouble(one, two, three) {
 
-    return one[0] + two[0] + three[0] + one[1] + two[1] + three[1]
+    
 
 }
 
