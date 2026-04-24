8b32ba9322603eb61b918a671550a7ee1cdc8b92
Colin Stanford <j.colin.stanford@gmail.com>
2026-04-23 19:26:11 -0600

    committing new challenge file with prep

---
+++ sumMixedArr.js
@@ -0,0 +1,25 @@
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+// Return your answer as a number.
+
+//PREP
+//Params
+// an array containing numbers and numbers as strings
+//Returns
+// sum of all values as if all nums
+//Examples
+// [9, 3, '7', '3'], 22;
+// ['5', '0', 9, 3, 2, 1, '9', 6, 7], 42; 
+// ['3', 6, 6, 0, '5', 8, 5, '6', 2,'0'], 41
+
+//Pseudocode
+// built-in Number() method to convert string to number
+// then simple reduce() method
+
+
+function sumMix(x) {
+
+
+
+}
+
+console.log(sumMix([9, 3, '7', '3']))
