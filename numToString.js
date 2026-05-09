0023d35672d674cebf2432895d7e0c06cd0e8336
Colin Stanford <j.colin.stanford@gmail.com>
2026-05-09 11:50:52 -0600

    committing new challenge with prep

---
+++ numToString.js
@@ -0,0 +1,23 @@
+// We need a function that can transform a number (integer) into a string.
+// What ways of achieving this do you know?
+
+//PREP
+//Params
+// given a number (integer)
+//Returns
+// return that number as a string
+// Examples (input --> output):
+    // 123  --> "123"
+    // 999  --> "999"
+    // -100 --> "-100"
+
+//Pseudocode
+
+
+function numberToString(num) {
+
+
+
+}
+
+console.log(numberToString(67));
