8e9852dc204bd8f7266d7307cb2863adad4223db
Colin Stanford <j.colin.stanford@gmail.com>
2026-06-02 14:52:03 -0600

    long conditional statement works fine

--- oppositeAttract.js
+++ oppositeAttract.js
@@ -20,11 +20,15 @@
 
 function lovefunc(flower1, flower2){
 
-
+    if (flower1 % 2 === 0 && flower2 % 2 !== 0 || flower2 % 2 === 0 && flower1 % 2 !== 0) {
+        return true;
+    } else {
+        return false;
+    }
 
 }
 
-console.log(lovefunc(2,3))
+console.log(lovefunc(2,6))
 console.log(lovefunc(1,4))
 console.log(lovefunc(0,1))
 console.log(lovefunc(1,1))
