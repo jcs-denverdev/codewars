48d77d5faa84c1429a601b8b111cd1fce8b159db
Colin Stanford <j.colin.stanford@gmail.com>
2026-06-03 14:48:21 -0600

    committing new challenge with prep

---
+++ oppositeAttract.js
@@ -0,0 +1,30 @@
+// Description:
+// Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+
+// Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+
+//Params:
+// given two flowers in the form of numbers
+//Returns:
+// true if one flower number is even and the other is odd
+//Examples:
+    // 1,4 ==> true
+    // 2,2 ==> false
+    // 0,1 ==> true
+
+//Pseudocode:
+// get started - conditional statements with or/and operators
+// if (flower1 % 2 === 0 && flower2 % 2 !== 0)
+// if (flower2 % 2 === 0 && flower1 % 2 !== 0)
+// this is repetitive
+
+function lovefunc(flower1, flower2){
+
+
+
+}
+
+console.log(lovefunc(2,3))
+console.log(lovefunc(1,4))
+console.log(lovefunc(0,1))
+console.log(lovefunc(1,1))
