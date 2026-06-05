950bdcefb8bfce0761143a0d2b5959fcfb0ac6c7
Colin Stanford <j.colin.stanford@gmail.com>
2026-06-05 14:20:25 -0600

    committing new challenge with prep

---
+++ oppositeNum.js
@@ -0,0 +1,20 @@
+// Description:
+// Very simple, given a number, find its opposite (additive inverse).
+
+//Params:
+// given a number
+//Returns:
+// return the inverse of given number
+
+// Examples:
+    // 1: -1
+    // 14: -14
+    // -34: 34
+
+//Pseudocode:
+// if number > 0, subtract number * 2
+// if number < 0, add number * 2
+
+function opposite(number) {
+
+}
