4e5269101f6d9d63c717544530944690cbe2419c
Colin Stanford <j.colin.stanford@gmail.com>
2026-08-01 10:40:08 -0600

    committing new challenge with prep

---
+++ countTheMonkeys.js
@@ -0,0 +1,20 @@
+// You take your son to the forest to see the monkeys. You know that there are a certain number there (n), but your son is too young to just appreciate the full number, he has to start counting them from 1.
+
+// As a good parent, you will sit and count with him. Given the number (n), populate an array with all numbers up to and including that number, but excluding zero.
+
+
+//Params
+// a number of monkeys n
+
+//Returns
+// populate an array with all numbers up to and including given number, excluding number
+
+//Examples
+    // 10 --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    //  1 --> [1]
+//Pseudocode
+// will need a for loop and an empty array to push into
+
+function monkeyCount (n) {
+    
+}
