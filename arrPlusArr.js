c770d8c28a823c4b2e2e19cca1a014be0d7e6af4
Colin Stanford <j.colin.stanford@gmail.com>
2026-08-28 14:09:10 -0600

    committing new array challenge with prep

---
+++ arrPlusArr.js
@@ -0,0 +1,17 @@
+// I'm new to coding and now I want to get the sum of two arrays... Actually the sum of all their elements. I'll appreciate for your help.
+
+// P.S. Each array includes only integer numbers. Output is a number too.
+
+//Params
+// two arrays
+//Returns
+// the sum of each element of the array plus the sum of each element of the second array
+//Examples
+    // [-1, -2, -3], [-4, -5, -6]) ==> -21
+    // [0, 0, 0], [4, 5, 6]) ==> 15
+//Pseudocode
+// 
+
+function arrayPlusArray(arr1, arr2) {
+
+}
