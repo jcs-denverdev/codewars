d4fe9a1d487ba969e0f6dc97f4ee4fe7d5399410
Colin Stanford <j.colin.stanford@gmail.com>
2026-09-01 13:31:03 -0600

    pseudocode based on solution after peeking at them

--- addLengthOfString.js
+++ addLengthOfString.js
@@ -13,11 +13,12 @@
     // "apple ban" --> ["apple 5", "ban 3"]
     // "you will win" -->["you 3", "will 4", "win 3"]
 //Pseudocode
-// 
+// split string of potentially multiple words into an array containing each word str.split(" ")
+// map() creates a new array populated with results of calling provided function on every element in the calling array
 
 function addLength(str) {
 
-
+    // str.split(" ")
 
 }
 
