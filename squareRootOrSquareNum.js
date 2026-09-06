87f2fc9f1ceb4478d6a2122d0d877ba0f9093d1b
Colin Stanford <j.colin.stanford@gmail.com>
2026-09-06 13:20:28 -0600

    committing new array challenge with prep

---
+++ squareRootOrSquareNum.js
@@ -0,0 +1,26 @@
+// Description:
+// Write a method, that will get an integer array as parameter and will process every number from this array.
+
+// Return a new array with processing every number of the input-array like this:
+// If the number has an integer square root, take this, otherwise square the number.
+
+// Notes
+// The input array will always contain only positive numbers, and will never be empty or null.
+
+//Params
+// an integer array
+//Returns
+// a new array with the same number of elements. if an element has an integer square root, replace it with that. otherwise, square the number
+//Examples
+    // [4,3,9,7,2,1] -> [2,9,3,49,4,1]
+//Pseudocode
+// 
+
+function squareOrSquareRoot(array) {
+
+
+
+}
+
+console.log(squareOrSquareRoot([4,3,9,7,2,1]))
+console.log(squareOrSquareRoot([100,101,5,5,1,1]))
